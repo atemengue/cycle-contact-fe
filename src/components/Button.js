6540f85ce28dd6eb43a5b42293925c0783a63db5
atemengue <moafembe@gmail.com>
2023-04-30 07:44:01 +0100

    add button component

---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  width: 100%;
+  height: 52px;
+  font-size: 16px;
+  color: #fff;
+  font-weight: bold;
+  border: none;
+  background: ${({ theme }) => theme.colors.primary.main};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+  transition: background 0.2s ease-in;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.primary.light}
+  }
+
+  &:active {
+    background: ${({ theme }) => theme.colors.primary.dark}
+  }
+
+  &[disabled]{
+    background #ccc;
+    cursor: default;
+    
+  }
+
+`;
