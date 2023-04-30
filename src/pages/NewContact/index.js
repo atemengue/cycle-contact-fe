66ec23189ed7baf7edfd376b98b301c6b73311a7
atemengue <moafembe@gmail.com>
2023-04-30 07:27:48 +0100

    add input & select commponent

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,5 +1,20 @@
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
 
 export default function NewContact() {
-  return <PageHeader title="Nouveau Contact" />;
+  return (
+    <>
+      <PageHeader title="Nouveau Contact" />
+      <Input placeholder="Nom" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+
+    </>
+  );
 }
